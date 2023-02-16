0147cfecbe3c809129463da79602e62ddcecef79
YuYoHan <zxzz8014@naver.com>
2023-02-16 13:38:00 +0900

    [Feat] UserServiceImpl_signUp

--- src/main/java/com/example/velog/service/UserServiceImpl.java
+++ src/main/java/com/example/velog/service/UserServiceImpl.java
@@ -1,6 +1,19 @@
 package com.example.velog.service;
 
+import com.example.velog.domain.UserDTO;
+import com.example.velog.mapper.UserMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService{
 
+    private UserMapper userMapper;
 
+    @Override
+    public boolean signUp(UserDTO userDTO) {
+        // 있으면 true
+        return userMapper.signUp(userDTO) == 1;
+    }
 }
