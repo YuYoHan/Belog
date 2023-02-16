6be681922b256461f4b235e373a6d986db3a78b0
YuYoHan <zxzz8014@naver.com>
2023-02-16 18:07:50 +0900

    [Feat] signUp_validation

--- src/main/java/com/example/velog/controller/UserController.java
+++ src/main/java/com/example/velog/controller/UserController.java
@@ -5,11 +5,15 @@ import com.example.velog.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 @Log4j2
@@ -24,7 +28,19 @@ public class UserController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(UserDTO userDTO, HttpServletResponse resp) {
+    public String signUp(@Validated UserDTO userDTO, Errors errors, HttpServletResponse resp, Model model) {
+        if(errors.hasErrors()) {
+            // 회원 가입 실패시, 입력 데이터를 유지
+            model.addAttribute("userDTO", userDTO);
+
+            // 유효성 통과 못한 필드와 메시지를 핸들링
+            Map<String, String> validatorResult = userService.validateHandling(errors);
+            for (String key: validatorResult.keySet()
+                 ) {
+                model.addAttribute(key,validatorResult.get(key));
+            }
+            return "/signUp";
+        }
         if(userService.signUp(userDTO)) {
             Cookie cookie = new Cookie("userEmail", userDTO.getUserEmail());
             // 30분
--- src/main/java/com/example/velog/service/UserService.java
+++ src/main/java/com/example/velog/service/UserService.java
@@ -1,7 +1,12 @@
 package com.example.velog.service;
 
 import com.example.velog.domain.UserDTO;
+import org.springframework.validation.Errors;
+
+import java.util.Map;
 
 public interface UserService {
     boolean signUp(UserDTO userDTO);
+
+    Map<String,String> validateHandling(Errors errors);
 }
