7434fd7fc6c9af0e7f2c77cec314349c1a0dbbe4
YuYoHan <zxzz8014@naver.com>
2023-02-16 13:33:20 +0900

    [Feat] UserDTO_signUp

---
+++ src/main/java/com/example/velog/domain/UserDTO.java
@@ -0,0 +1,18 @@
+package com.example.velog.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private Long userId;
+    private String userEmail;
+    private String userPw;
+    private String userName;
+    private String userAddr;
+    private MultipartFile userImg;
+}
