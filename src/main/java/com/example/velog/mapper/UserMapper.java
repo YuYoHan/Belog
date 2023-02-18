fd3687ce36b5506a301a29237e79dfd4b3e11a12
YuYoHan <zxzz8014@naver.com>
2023-02-18 20:14:16 +0900

    [Feat] UserMapper_login

--- src/main/java/com/example/velog/mapper/UserMapper.java
+++ src/main/java/com/example/velog/mapper/UserMapper.java
@@ -2,8 +2,13 @@ package com.example.velog.mapper;
 
 import com.example.velog.domain.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
     int signUp(UserDTO userDTO);
+
+    // mybatis는 2개 이상을 보낼 때는 dto로 묶어주던지
+    // 아니면 param으로 처리
+    UserDTO login(@Param("userId") String userId, @Param("userPw") String userPw);
 }
