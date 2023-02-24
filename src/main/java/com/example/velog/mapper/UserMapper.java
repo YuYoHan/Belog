f717c12ef28a6db3e7cb73311c9ccae8e0df43ee
YuYoHan <zxzz8014@naver.com>
2023-02-24 19:21:20 +0900

    [Docs] UserMapper

--- src/main/java/com/example/velog/mapper/UserMapper.java
+++ src/main/java/com/example/velog/mapper/UserMapper.java
@@ -10,7 +10,7 @@ public interface UserMapper {
 
     // mybatis는 2개 이상을 보낼 때는 dto로 묶어주던지
     // 아니면 param으로 처리
-    UserDTO login(@Param("userId") String userId, @Param("userPw") String userPw);
+    UserDTO login(@Param("userEmail") String userEmail, @Param("userPw") String userPw);
 
-    UserDTO deleteUser(String userEmail, String userPw);
+    UserDTO deleteUser(@Param("userEmail") String userEmail, @Param("userPw") String userPw);
 }
