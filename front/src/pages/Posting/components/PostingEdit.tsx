71c332bbbb4f89468ed10b2674f48fbf46d9de31
cleooo5857 <zleooo@naver.com>
2023-03-11 16:47:20 +0900

    [Refactor] api 로직 분리 가독성 추가

--- front/src/pages/Posting/components/PostingEdit.tsx
+++ front/src/pages/Posting/components/PostingEdit.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import useInput from 'hooks/useInput';
 import React, { ChangeEvent, useEffect, useMemo, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -5,12 +7,26 @@ import styled from 'styled-components';
 
 function PostingEdit() {
   
-  
-   const [value, setValue] = useState('');
-   const quillRef = useRef();
+   const [content, setcontent ] = useInput('');
+   const QuillRef = useRef<ReactQuill>();
 
-  console.log(value);
-  
+
+   
+   // useEffect(() => {
+   //    axios.post('https://2432b997-c1de-4c6d-a6a7-36b68e210341.mock.pstmn.io/post', {
+         
+   //       lastName: 'Flintstone'
+   //     })
+   //     .then(function (response) {
+   //       console.log(response);
+   //     })
+   //     .catch(function (error) {
+   //       console.log(error);
+   //     });
+   //  }, []);
+    
+    console.log(content);
+    
 //   const toolbarOptions = {
 //    toolbar: {
 //      container: [
@@ -33,7 +49,6 @@ function PostingEdit() {
 
 const imageHandler = () => {
    console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
- 
    // 1. 이미지를 저장할 input type=file DOM을 만든다.
    const input = document.createElement('input');
       input.setAttribute('type', 'file');
@@ -52,30 +67,34 @@ const imageHandler = () => {
         // multer에 맞는 형식으로 데이터 만들어준다.
         const formData = new FormData();
         formData.append('img', file); // formData는 키-밸류 구조
-     // 백엔드 multer라우터에 이미지를 보낸다.
-   //   try {
-   //    //  const result = await axios.post('http://localhost:4050/img', formData);
-   //     console.log('성공 시, 백엔드가 보내주는 데이터', result.data.url);
-   //     const IMG_URL = result.data.url;
-   //     // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
-   //     // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
-   //     // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+        // 백엔드 multer라우터에 이미지를 보낸다.
+     try {
+       const result = await axios.post('posts', formData);
+       console.log('성공 시, 백엔드가 보내주는 데이터', result.data.url);
+       const IMG_URL = result.data.url;
+       
+       // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+       // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+       // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
  
-   //     // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
-   //     const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
-   //     // 1. 에디터 root의 innerHTML을 수정해주기
-   //     // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
-   //     // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
-   //     // editor.root.innerHTML =
-   //     //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
- 
-   //     // 2. 현재 에디터 커서 위치값을 가져온다
-   //     const range = editor.getSelection();
-   //     // 가져온 위치에 이미지를 삽입한다
-   //     editor.insertEmbed(range.index, 'image', IMG_URL);
-   //   } catch (error) {
-   //     console.log('실패했어요ㅠ');
-   //   }
+       // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+       const editor = QuillRef.current?.getEditor(); // 에디터 객체 가져오기
+       console.log(editor);
+       
+       // 1. 에디터 root의 innerHTML을 수정해주기
+       // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+       // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+         if(editor){
+            editor.root.innerHTML =
+            editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+         }
+       // 2. 현재 에디터 커서 위치값을 가져온다
+         //  const range = editor.getSelection();
+         //  editor.insertEmbed(range.index, 'image', IMG_URL);
+       // 가져온 위치에 이미지를 삽입한다
+     } catch (error) {
+       console.log('실패했어요ㅠ');
+     }
    });
  };
 
@@ -111,12 +130,17 @@ const modules = useMemo(() => {
 
   return (
    <S.Wrapper>
-      <ReactQuill 
+      <ReactQuill
+      ref={(element) => {
+         if (element !== null) {
+           QuillRef.current = element;
+         }
+       }}
       theme="snow" 
-      value={value}
+      value={content}
       modules={modules}
       formats={formats}
-      onChange={setValue}
+      onChange={setcontent}
       placeholder="내용을 입력해주세요"/>;
    </S.Wrapper>
   )
