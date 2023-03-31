b2f1345bc5eb29a467b2a2c9ea979ddcad694236
dahyunJung <deh2246@naver.com>
2023-04-01 03:19:15 +0900

    [Feat]Rest api로 수정

--- src/main/java/com/example/Belog/controller/CommentController.java
+++ src/main/java/com/example/Belog/controller/CommentController.java
@@ -4,6 +4,8 @@ import com.example.Belog.domain.CommentDTO;
 import com.example.Belog.service.CommentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,90 +14,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-@Controller
+@RestController
 @Log4j2
 @AllArgsConstructor
+@RequestMapping("/board/{boradNum}")
 public class CommentController {
 
     private CommentService commentService;
 
     // 댓글작성
-    @PostMapping("/comment/add")    //?
-    public String addComment(@RequestParam String comment, HttpServletRequest request) {
-        log.info("add Comment");
-
-        // userId 세션에서 값 얻어오기
-        HttpSession session = request.getSession();
-        Long userId = (Long) session.getAttribute("userId");
-
-        if (userId != null) {
-            //boardNum을 얻어오는 값
-            Long boardNum = Long.parseLong(request.getParameter("boardNum"));
-
-            try {
-                if (!comment.equals("") && !comment.isEmpty()) {
-                    CommentDTO commentDTO = CommentDTO.builder()
-                            .userId(userId)
-                            .boardNum(boardNum)
-                            .comment(comment)
-                            .build();
-                    commentService.addComment(commentDTO);
-                }
-                log.info("Add success");
-            } catch (Exception e) {
-                log.info("error");
-                e.printStackTrace();
-            }
-        }
-        return "redirect:/board/{boardNum}";
+    @PostMapping("/comment")
+    public void addComment(@RequestBody CommentDTO commentDTO) {
+        commentService.addComment(commentDTO);
+        log.info("Add success");
     }// addComment
 
     // 댓글수정
-    @PostMapping("/comment/edit")
-    public String editComment(@RequestParam String comment, HttpServletRequest request) {
-        Long boardNum = Long.parseLong(request.getParameter("boardNum"));
-        Long commentNum = Long.parseLong(request.getParameter("commentNum"));
-
-        CommentDTO commentDTO = CommentDTO.builder()
-                .boardNum(boardNum)
-                .commentNum(commentNum)
-                .comment(comment)
-                .build();
-
+    @PutMapping("/{commentNum}")
+    public CommentDTO editComment(@PathVariable("commentNum") Long commentNum, @RequestBody CommentDTO commentDTO) {
         commentService.editComment(commentDTO);
         log.info("edit");
-        return "redirect:/board/{boardNum}";
-    }
+        return commentDTO;
+    }//editComment
 
     // 댓글삭제
-    @PostMapping("/comment/delete")
-    public String deleteComment(HttpServletRequest request) {
-        Long boardNum = Long.parseLong(request.getParameter("boardNum"));
-        Long commentNum = Long.parseLong(request.getParameter("commentNum"));
-
-        CommentDTO commentDTO = CommentDTO.builder()
-                .boardNum(boardNum)
-                .commentNum(commentNum)
-                .build();
-
+    @DeleteMapping("/{commentNum}")
+    public CommentDTO deleteComment(@PathVariable("commentNum") Long commentNum, @RequestBody CommentDTO commentDTO) {
         commentService.deleteComment(commentDTO);
         log.info("delete");
-        return "redirect:/board/{boardNum}";
-    }
+        return commentDTO;
+    }//deleteComment
 
     // 전체 댓글보여주기
-    @GetMapping("/comment/{boardNum}")
-    public String findAllComment(@PathVariable Long boardNum, HttpServletRequest request, Model model) {
-        log.info("find");
-
+    @GetMapping("/commentList")
+    public List<CommentDTO> findAllComment(@PathVariable("boardNum") Long boardNum) {
         List<CommentDTO> commentList = commentService.findAllComment(boardNum);
-        model.addAttribute("commentList", commentList);
+        log.info("find");
+        return commentList;
+    }//findAllComment
 
-        return "redirect:/board/{boardNum}";
-    }
-
-    @GetMapping("/comment/count")
-    public int count(int boardNum) {
+    //댓글 수
+    @GetMapping("/countComment")
+    public int count(@PathVariable("boardNum") int boardNum) {
         return commentService.countComment(boardNum);
     }
 }
