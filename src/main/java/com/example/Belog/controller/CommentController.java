27e076c992c1cabe59f565df6d76dbdbe02cd9a0
dahyunJung <deh2246@naver.com>
2023-04-08 09:45:47 +0900

    [Feat] commentController @RequestBody 추가

--- src/main/java/com/example/Belog/controller/CommentController.java
+++ src/main/java/com/example/Belog/controller/CommentController.java
@@ -28,42 +28,39 @@ public class CommentController {
 
     private CommentService commentService;
 
-    @Tag(name = "Comment check")
-    @Operation(summary = "추가 API", description = "댓글을 추가하는 API입니다.")
     // 댓글작성
+    @Tag(name = "Comment check")  //API 그룹설정
+    @Operation(summary = "추가 API", description = "댓글을 추가하는 API입니다.") // API 설명
     @PostMapping("/comment")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "댓글 등록 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글 등록 실패")
-    })
-    public ResponseEntity<?> addComment(CommentDTO commentDTO) {
+            @ApiResponse(responseCode = "404", description = "댓글 등록 실패")}) //API 호출결과에 대해서 설명
+    public ResponseEntity<?> addComment(@RequestBody CommentDTO commentDTO) {
         commentService.addComment(commentDTO);
         log.info("Add success");
         return new ResponseEntity<>(commentDTO, HttpStatus.CREATED);
     }// addComment
 
+    // 댓글수정
     @Tag(name = "Comment check")
     @Operation(summary = "수정 API", description = "댓글을 수정하는 API입니다.")
-    // 댓글수정
     @PutMapping("/comment/{commentNum}")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "댓글 수정 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글 수정 실패")
-    })
-    public ResponseEntity<?> editComment(CommentDTO commentDTO) {
+            @ApiResponse(responseCode = "404", description = "댓글 수정 실패") })
+    public ResponseEntity<?> editComment(@RequestBody CommentDTO commentDTO) {
         commentService.editComment(commentDTO);
         log.info("edit");
         return new ResponseEntity<>(commentDTO, HttpStatus.OK);
     }//editComment
 
+    // 댓글삭제
     @Tag(name = "Comment check")
     @Operation(summary = "삭제 API", description = "댓글을 삭제하는 API입니다.")
-    // 댓글삭제
     @DeleteMapping("/comment/{commentNum}")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "댓글 삭제 성공"),
-            @ApiResponse(responseCode = "404", description = "댓글 삭제 실패")
-    })
+            @ApiResponse(responseCode = "404", description = "댓글 삭제 실패")})
     public ResponseEntity<?> deleteComment(@PathVariable("boardNum") Long boardNum,
                                            @PathVariable("commentNum") Long commentNum) {
         commentService.deleteComment(commentNum);
@@ -79,9 +76,9 @@ public class CommentController {
 //        return commentService.findComment(boardNum, commentNum);
 //    }//deleteComment
 
+    // 전체 댓글보여주기
     @Tag(name = "Comment check")
     @Operation(summary = "전체 조회 API", description = "댓글을 모두 조회하는 API입니다.")
-    // 전체 댓글보여주기
     @GetMapping("/comment/list")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "전체 댓글 조회 성공"),
@@ -92,9 +89,9 @@ public class CommentController {
         return commentService.findAllComment(boardNum);
     }//findAllComment
 
+    //댓글 수
     @Tag(name = "Comment check")
     @Operation(summary = "댓글 갯 수 API", description = "댓글수에 관련된 API입니다.")
-    //댓글 수
     @GetMapping("/comment/count")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "전체 댓글 수 조회 성공"),
