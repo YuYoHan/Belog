9730f978625dc74234063330f92abbf62a307eba
YuYoHan <zxzz8014@naver.com>
2023-04-16 15:10:22 +0900

    [Test] UserServiceTest

--- src/test/java/com/example/Belog/service/UserServiceTest.java
+++ src/test/java/com/example/Belog/service/UserServiceTest.java
@@ -1,21 +1,27 @@
 package com.example.Belog.service;
 
 import com.example.Belog.domain.UserDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
-// H2 데이터베이스 사용하기 위해서 설정
-@TestPropertySource(locations = "classpath:application-prod.properties")
+@Slf4j
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
 
     @Test
     @DisplayName("회원가입 테스트")
     void signUp() {
-        String email = "zxzz45@naver.com";
+        String email = "zxzz46@naver.com";
         String password = "Dbekdms147!";
         String name = "테스터";
         String addr = "서울시 xxxx xxx";
@@ -31,23 +37,80 @@ class UserServiceTest {
                 .userAddrEtc(addrEtc)
                 .build();
 
-        Assertions.assertThat(p1.getUserEmail()).isEqualTo(email);
-        Assertions.assertThat(p1.getUserPw()).isEqualTo(password);
-        Assertions.assertThat(p1.getUserName()).isEqualTo(name);
-        Assertions.assertThat(p1.getUserAddr()).isEqualTo(addr);
+        if(userService.signUp(p1)) {
+            log.info(p1.getUserEmail());
+            log.info(p1.getUserPw());
+            log.info(p1.getUserName());
+            log.info(p1.getUserAddr());
+            log.info(p1.getUserAddrDetail());
+            log.info(p1.getUserAddrEtc());
 
+            Assertions.assertThat(p1.getUserEmail()).isEqualTo(email);
+            Assertions.assertThat(p1.getUserPw()).isEqualTo(password);
+            Assertions.assertThat(p1.getUserName()).isEqualTo(name);
+            Assertions.assertThat(p1.getUserAddr()).isEqualTo(addr);
+            Assertions.assertThat(p1.getUserAddrDetail()).isEqualTo(addrDetail);
+            Assertions.assertThat(p1.getUserAddrEtc()).isEqualTo(addrEtc);
+        }
     }
 
     @Test
     @DisplayName("로그인 테스트")
     public void  login() {
         UserDTO p2 = UserDTO.builder()
-                .userEmail("zxzz45@naver.com")
+                .userEmail("zxzz46@naver.com")
                 .userPw("Dbekdms147!")
                 .build();
 
-        Assertions.assertThat(p2.getUserEmail()).isEqualTo("zxzz45@naver.com");
-        Assertions.assertThat(p2.getUserPw()).isEqualTo("Dbekdms147!");
+        UserDTO loginUser = userService.login(p2.getUserEmail(), p2.getUserPw());
+
+        log.info(p2.getUserEmail());
+        log.info(p2.getUserPw());
+        Assertions.assertThat(p2.getUserEmail()).isEqualTo(loginUser.getUserEmail());
+        Assertions.assertThat(p2.getUserPw()).isEqualTo(loginUser.getUserPw());
+    }
+
+    @Test
+    @DisplayName("이메일 중복 체크 테스트")
+    void emailCheck() {
+        UserDTO p3 = UserDTO.builder()
+                .userEmail("zxzz48@naver.com")
+                .build();
+
+        int userCheck = userService.emailCheck(p3.getUserEmail());
+
+        if(userCheck == 1) {
+            log.error("아이디 중복입니다.");
+            Assertions.assertThat(p3.getUserEmail()).isEqualTo("zxzz45@naver.com");
+        } else {
+            log.info("아이디를 사용할 수 있습니다.");
+        }
+    }
+
+    @Test
+    @DisplayName("회원 조회 테스트")
+    void findByUser() {
+        String email = "zxzz46@naver.com";
+
+        UserDTO user = userService.getUser(email);
+        log.info(String.valueOf(user));
+
+        Assertions.assertThat(user.getUserEmail()).isEqualTo(email);
+    }
+
+    @Test
+    @DisplayName("전체 회원 조회 테스트")
+    void findByAllUser() {
+        List<UserDTO> userList = userService.getAllUser();
+        for (UserDTO user: userList
+             ) {
+            log.info("user = {}", user);
+        }
+    }
+
+    @Test
+    @DisplayName("업데이트 테스트")
+    void updateUser() {
 
     }
 }
