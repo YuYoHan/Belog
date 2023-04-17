7cd32447ee351282ea8ff06eaaa0d0f6c7da483a
YuYoHan <zxzz8014@naver.com>
2023-04-17 15:08:53 +0900

    [Rename] GlobalExceptionHandler 이름 오타 수정

--- src/main/java/com/example/Belog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Belog/exception/GlobalExceptionHandler.java
@@ -3,24 +3,23 @@ package com.example.Belog.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExcepttionHandler {
+public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handlerException(Exception exception, Model model) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception);
     }
 
-    @ExceptionHandler(UserException.class )
+    @ExceptionHandler(UserNotFoundException.class )
     public ResponseEntity<UserException> handleCustomException(UserException userException, Model model) {
         model.addAttribute("userException", userException);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userException);
     }
 
-    @ExceptionHandler(BoardException.class)
+    @ExceptionHandler(BoardNotFoundException.class)
     public ResponseEntity<BoardException> handleCustomException2(BoardException boardException, Model model) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(boardException);
     }
