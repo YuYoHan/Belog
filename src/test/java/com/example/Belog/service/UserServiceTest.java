bf79ec18aa1f5c4f1088c6ca8f66d6236c0a716a
YuYoHan <zxzz8014@naver.com>
2023-04-20 13:36:03 +0900

    [Test] test 추가

--- src/test/java/com/example/Belog/service/UserServiceTest.java
+++ src/test/java/com/example/Belog/service/UserServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @Slf4j
